2806f09b6a1cdac879234309fe0ccd0fbcc4a383
Quombus <beckettrichardmatthew@gmail.com>
2024-09-22 14:40:14 -0500

    implemented findByValue and added case insensitivity to user search

--- src/main/java/JobData.java
+++ src/main/java/JobData.java
@@ -6,7 +6,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
@@ -72,10 +71,9 @@ public class JobData {
         ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
 
         for (HashMap<String, String> row : allJobs) {
-
             String aValue = row.get(column);
 
-            if (aValue.contains(value)) {
+            if ((aValue.toLowerCase()).contains(value)) {
                 jobs.add(row);
             }
         }
@@ -95,7 +93,20 @@ public class JobData {
         loadData();
 
         // TODO - implement this method
-        return null;
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+
+
+        for (HashMap<String, String> row : allJobs) {
+
+            if ((row.get("core competency").toLowerCase()).contains(value) ||
+                    (row.get("employer").toLowerCase()).contains(value) ||
+                    (row.get("location").toLowerCase()).contains(value) ||
+                    (row.get("position type").toLowerCase()).contains(value)) {
+                jobs.add(row);
+            }
+        }
+
+        return jobs;
     }
 
     /**
